9bfa1873e41b1e59505bc9158769417c4a3e608b
mskyberg <mskyberg@gmail.com>
2020-06-04 18:22:53 -0500

    added test for score 2 negative input

--- test/test_validation_with_try.py
+++ test/test_validation_with_try.py
@@ -16,6 +16,10 @@ class MyTestCase(unittest.TestCase):
         with self.assertRaises(ValueError):
             avg.average(-90, 89, 78)
 
+    def test_average_negative_input(self):
+        with self.assertRaises(ValueError):
+            avg.average(90, -89, 78)
+
 
 if __name__ == '__main__':
     unittest.main()
